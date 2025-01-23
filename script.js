79ee3cb26e8b068c9bac6f066e455292d1800b27
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2025-01-23 12:26:45 +0800

    Create method to remove book

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ closeButton.addEventListener('click', closeDialog);
 dialog.addEventListener('close', getFormValues);
 
 const myLibrary = [];
+let bookID = 0;
 
 // create book constructor
 function Book(title, author, pages, read) {
@@ -17,28 +18,40 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.bookID = bookID;
 }
 
 // create function that adds books to library, after creating them with constructor
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
-    myLibrary.push(book);
     displayBook(book);
+    myLibrary.push(book);
+    bookID++;
 }
 
 // show books as cards
 function displayBook(book) {
     const div = document.createElement('div');
-    div.classList.toggle('card');
+    div.classList.toggle(`card`);
+    div.id = `book-${bookID}`;
 
     for (const property in book) {
         const p = document.createElement('p');
         if (property === 'read')
             book.read ? p.textContent = 'Read' : p.textContent = 'Unread';
+        else if (property === 'bookID')
+            continue;
         else
             p.textContent = book[property];
+        p.classList.toggle(property);
         div.appendChild(p);
     }
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.classList.toggle('remove');
+    removeButton.addEventListener('click', removeBookFromLibrary);
+    div.appendChild(removeButton);
 
     const content = document.querySelector('.content');
     content.appendChild(div);
@@ -61,3 +74,18 @@ function closeDialog(event) {
     form.reset()
     addBookCancelled = true;
 }
+
+function removeBookFromLibrary(event) {
+    // remove element from DOM
+    const targetCard = event.target.parentElement.id;
+    const targetBook = targetCard.split('|')[1];
+    targetCard.remove();
+
+    // remove element from myLibrary
+    for (let i = 0, length = myLibrary.length; i < length; i++) {
+        if (myLibrary[i].bookID === targetBook) myLibrary.splice[i, 1];
+    }
+}
+
+const remove = document.querySelector('.remove');
+remove.addEventListener('click', removeBookFromLibrary);
--- test.js
+++
@@ -1,5 +0,0 @@
-test();
-
-function test() {
-    console.log('i work');
-}
